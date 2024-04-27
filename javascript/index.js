f125bbf342c057795a66611dd32b7fabe0af1eb7
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-04-27 19:48:07 +0700

    finish the base

--- javascript/index.js
+++ javascript/index.js
@@ -24,34 +24,95 @@ class Tetris {
         return true;
     }
 
-
     getTruncedPosition() {
-        return { x: Math.trunc(this.x), y: Math.trunc(this.y) }
+        return { x: Math.trunc(this.x), y: Math.trunc(this.y) };
     }
-
-
     checkLeft() {
+        for (let i = 0; i < this.template.length; i++) {
+            for (let j = 0; j < this.template.length; j++) {
+                if (this.template[i][j] == 0) continue;
+                let realX = i + this.getTruncedPosition().x;
+                let realY = j + this.getTruncedPosition().y;
+                if (realX - 1 < 0) {
+                    return false;
+                }
 
+                if (gameMap[realY][realX - 1].imageX != -1) return false;
+            }
+        }
+        return true;
     }
 
     checkRight() {
+        for (let i = 0; i < this.template.length; i++) {
+            for (let j = 0; j < this.template.length; j++) {
+                if (this.template[i][j] == 0) continue;
+                let realX = i + this.getTruncedPosition().x;
+                let realY = j + this.getTruncedPosition().y;
+                if (realX + 1 >= squareCountX) {
+                    return false;
+                }
 
+                if (gameMap[realY][realX + 1].imageX != -1) return false;
+            }
+        }
+        return true;
     }
 
     moveRight() {
-
+        if (this.checkRight()) {
+            this.x += 1;
+        }
     }
 
     moveLeft() {
-
+        if (this.checkLeft()) {
+            this.x -= 1;
+        }
     }
 
     moveBottom() {
-
+        if (this.checkBottom()) {
+            this.y += 1;
+            score += 1;
+        }
     }
-
     changeRotation() {
+        let tempTemplate = [];
+        for (let i = 0; i < this.template.length; i++)
+            tempTemplate[i] = this.template[i].slice();
+        let n = this.template.length;
+        for (let layer = 0; layer < n / 2; layer++) {
+            let first = layer;
+            let last = n - 1 - layer;
+            for (let i = first; i < last; i++) {
+                let offset = i - first;
+                let top = this.template[first][i];
+                this.template[first][i] = this.template[i][last]; // top = right
+                this.template[i][last] = this.template[last][last - offset]; //right = bottom
+                this.template[last][last - offset] =
+                    this.template[last - offset][first];
+                //bottom = left
+                this.template[last - offset][first] = top; // left = top
+            }
+        }
 
+        for (let i = 0; i < this.template.length; i++) {
+            for (let j = 0; j < this.template.length; j++) {
+                if (this.template[i][j] == 0) continue;
+                let realX = i + this.getTruncedPosition().x;
+                let realY = j + this.getTruncedPosition().y;
+                if (
+                    realX < 0 ||
+                    realX >= squareCountX ||
+                    realY < 0 ||
+                    realY >= squareCountY
+                ) {
+                    this.template = tempTemplate;
+                    return false;
+                }
+            }
+        }
     }
 
 }
@@ -62,10 +123,12 @@ const framePerSecond = 60;
 const gameSpeed = 5;
 let whiteLineThickness = 4;
 const canvas = document.getElementById('canvas');
-
+const nextShapeCanvas = document.getElementById("nextShapeCanvas");
 const image = document.getElementById('image');
 
 const ctx = canvas.getContext("2d");
+const nctx = nextShapeCanvas.getContext("2d");
+
 
 const squareCountX = canvas.width / size;
 const squareCountY = canvas.height / size;
@@ -120,10 +183,50 @@ let gameLoop = () => {
     setInterval(draw, 1000 / framePerSecond);
 }
 
+let deleteCompleteRows = () => {
+    for (let i = 0; i < gameMap.length; i++) {
+        let t = gameMap[i];
+        let isComplete = true;
+        for (let j = 0; j < t.length; j++) {
+            if (t[j].imageX == -1) isComplete = false;
+        }
+        if (isComplete) {
+            console.log("complete row");
+            score += 1000;
+            for (let k = i; k > 0; k--) {
+                gameMap[k] = gameMap[k - 1];
+            }
+            let temp = [];
+            for (let j = 0; j < squareCountX; j++) {
+                temp.push({ imageX: -1, imageY: -1 });
+            }
+            gameMap[0] = temp;
+        }
+    }
+};
+
 let update = () => {
     if (gameOver) return;
     if (currentShape.checkBottom()) {
         currentShape.y += 1;
+    }
+    else {
+        for (let k = 0; k < currentShape.template.length; k++) {
+            for (let l = 0; l < currentShape.template.length; l++) {
+                if (currentShape.template[k][l] == 0) continue;
+                gameMap[
+                    currentShape.getTruncedPosition().y + l
+                ][
+                    currentShape.getTruncedPosition().x + k
+                ] = { imageX: currentShape.imageX, imageY: currentShape.imageY };
+            }
+        }
+        deleteCompleteRows();
+        currentShape = nextShape;
+        nextShape = getRandomShape();
+        if (!currentShape.checkBottom()) {
+            gameOver = true;
+        }
     }
 }
 
@@ -163,16 +266,62 @@ let drawCurrentTetris = () => {
 };
 
 let drawSquares = () => {
+    for (let i = 0; i < gameMap.length; i++) {
+        let t = gameMap[i];
+        for (let j = 0; j < gameMap[i].length; j++) {
+            if (t[j].imageX == -1) continue;
+            ctx.drawImage(
+                image,
+                t[j].imageX,
+                t[j].imageY,
+                imageSquareSize,
+                imageSquareSize,
+                j * size,
+                i * size,
+                size,
+                size
+            )
+        }
+    }
 }
 
 let drawNextShape = () => {
+    nctx.fillStyle = "#bca0dc";
+    nctx.fillRect(0, 0, nextShapeCanvas.width, nextShapeCanvas.height);
+    for (let i = 0; i < nextShape.template.length; i++) {
+        for (let j = 0; j < nextShape.template.length; j++) {
+            if (nextShape.template[i][j] == 0) continue;
+            nctx.drawImage(
+                image,
+                nextShape.imageX,
+                nextShape.imageY,
+                imageSquareSize,
+                imageSquareSize,
+                size * i,
+                size * j + size,
+                size,
+                size
+            );
+        }
+    }
+};
 
-}
 
+
+
+let drawScore = () => {
+    sctx.clearRect(0, 0, scoreCanvas.width, scoreCanvas.height);
+    sctx.font = "64px Poppins";
+    sctx.fillStyle = "white";
+    sctx.fillText(score, 10, 50);
+};
 
 let drawGameOver = () => {
+    ctx.font = "64px Poppins";
+    ctx.fillStyle = "white";
+    ctx.fillText("Game Over!", 10, canvas.height / 2);
+};
 
-}
 
 let draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -184,6 +333,8 @@ let draw = () => {
         drawGameOver();
     }
 }
+
+
 
 let getRandomShape = () => {
     return Object.create(shapes[Math.floor(Math.random() * shapes.length)]);
@@ -210,6 +361,7 @@ window.addEventListener("keydown", (event) => {
     else if (event.keyCode == 39) currentShape.moveRight();
     else if (event.keyCode == 40) currentShape.moveBottom();
 });
+
 
 resetVars();
 gameLoop();
